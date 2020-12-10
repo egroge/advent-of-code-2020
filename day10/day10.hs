5855ece388c53f8017e4c7888ba6b901b4171c7d
egroge <egroge.stacey@gmail.com>
2020-12-10 15:24:29 +0000

    Updated to use State. Still messy

--- day10/day10.hs
+++ day10/day10.hs
@@ -1,6 +1,7 @@
 import Data.List (sort)
 import Data.Map (Map, (!?), empty, insert, singleton, union, size)
 import Data.Maybe
+import Control.Monad.State
 
 type Jolt = Int 
 
@@ -18,30 +19,32 @@ getJolts = return . map read . lines
 
 -- PRE: js is sorted and includes starting 0 and final max + 3
 combs :: [Jolt] -> Int
-combs js = fst (go js empty)
-  where 
-    -- TODO this is too messy
-    go :: [Int] -> Map [Int] Int -> (Int, Map [Int] Int)
-    go js@(j : j' : j'' : js') m
-      | diff <= 3 && diff >= 1 = (combined, insert js combined mCombined)
-      | otherwise              = (nKept, insert js nKept mKept)
-      where
-        diff = j'' - j
-        combined    = nKept + nDiscarded
-        mCombined   = mKept `union` mDiscarded
-        keptJs      = j' : j'' : js'
-        discardedJs = j : j'' : js'
-
-        (nKept, keptMap)           = case m !? keptJs of 
-                                      Just n  -> (n, m)
-                                      Nothing -> go keptJs m
-
-        (nDiscarded, discardedMap) = case keptMap !? discardedJs of 
-                                      Just n  -> (n, m)
-                                      Nothing -> go discardedJs keptMap 
-    go js m = case m !? js of
-                Just n  -> (n, m)
-                Nothing -> (1, singleton js 1)
+combs js = evalState (combs' js) empty
+  where
+    combs' :: [Int] -> State (Map [Int] Int) Int
+    combs' js@(j : _ : j' : js') = do 
+        m <- get
+        case m !? js of 
+          Just n  -> return n 
+          Nothing -> do
+            nKept <- combs' (tail js)
+           
+            if j' - j <= 3 
+            then do 
+              nDiscarded <- combs' (j : j' : js')
+              let both = nDiscarded + nKept
+              m' <- get 
+              put (insert js both m')
+              return both
+            else do 
+              m' <- get
+              put (insert js nKept m')
+              return nKept 
+    combs' js = do
+      m <- get 
+      case m !? js of 
+        Just n  -> return n 
+        Nothing -> put (singleton js 1) >> return 1
 
 partTwo :: [Jolt] -> Int 
 partTwo js = combs longest
